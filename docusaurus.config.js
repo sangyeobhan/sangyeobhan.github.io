ed2906af622be3f181b9dc503d5910043af6e829
sangyeobhan <sangyeobhandev@gmail.com>
2025-12-22 11:36:59 +0900

    url 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sangyeobhan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sangyeobhan', // Usually your GitHub org/user name.
-  projectName: 'blog', // Usually your repo name.
+  projectName: 'sangyeobhan.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
